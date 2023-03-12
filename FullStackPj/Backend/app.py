c324f9b0c20844d6e3ae198813be7caee98db274
Kindaglia <matteodrago007@gmail.com>
2023-03-12 19:36:51 +0100

    add backend

--- FullStackPj/Backend/app.py
+++ FullStackPj/Backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from pymongo import MongoClient
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "test"
+def index():
+    return 'docker works!'
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
