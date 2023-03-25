0aac58ed9fe823da1529d91508fcfd9db324c039
Kindaglia <matteodrago007@gmail.com>
2023-03-25 12:27:13 +0100

    add ful example fe

--- FullStackPj/Frontend/src/app/app.component.ts
+++ FullStackPj/Frontend/src/app/app.component.ts
@@ -9,11 +9,29 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'Frontend';
   documents: any[] = [];
+  newDocument: any = {};
 
   constructor(private http: HttpClient) {
+    this.fetchDocuments();
+  }
+
+  fetchDocuments() {
     this.http.get<any[]>('http://localhost:5000/documento').subscribe((data) => {
       this.documents = data;
       console.log(data);
+    });
+  }
+
+  addDocument() {
+    this.http.post('http://localhost:5000/documento', this.newDocument).subscribe(() => {
+      this.fetchDocuments();
+      this.newDocument = {}; // reset the form
+    });
+  }
+
+  deleteDocument(id: string) {
+    this.http.delete(`http://localhost:5000/documento/${id}`).subscribe(() => {
+      this.fetchDocuments();
     });
   }
 }
