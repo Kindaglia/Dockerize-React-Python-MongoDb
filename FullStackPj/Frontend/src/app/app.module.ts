88b75c8034d3d5ebab075153134de6df07b6f100
Kindaglia <matteodrago007@gmail.com>
2023-03-25 17:09:07 +0100

    update

--- FullStackPj/Frontend/src/app/app.module.ts
+++ FullStackPj/Frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,8 +13,10 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+
     FormsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule // Aggiungi questa riga
   ],
   providers: [],
